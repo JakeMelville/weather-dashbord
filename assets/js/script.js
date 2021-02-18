6c9ce7b4138c53acffd09618ebc3eefafcfef198
Jake Melville <jakemelville@ymail.com>
2021-02-17 22:44:25 -0500

    added javascript. Connected to open weather api and ran fetch. Now when the user types in a city all data shows up in console. Data in an object

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,43 @@
 var searchBtn = document.querySelector(".btn");
+var baseUrl = "https://api.openweathermap.org/data/2.5/forecast?q=";
+var apiKey = "&appid=c325d98bc2a304d27df0719cfbbccb4f";
+var units = "&units=imperial";
+var currentConditions = document.getElementById("current-conditions");
+var temp = document.getElementById("current");
 
-function handleSearchFormSubmit(event) {
+
+function locationSelect(event) {
   event.preventDefault();
 
   var searchInput = document.querySelector('#search-input').value;
 
-  if (!searchInputVal) {
-    console.error('Please select a location');
+  if (!searchInput) {
+    confirm('Please select a location');
     return;
   }
 
-  var queryString = './search-results.html?q=' + searchInput;
+  var queryString = baseUrl + searchInput + units + apiKey;
+  console.log(queryString)
 
-  location.assign(queryString);
+  fetch(queryString) 
+    .then(response => response.json()) //converting the response to json
+    //.then((weatherData) => {     -->another way to write the above function!
+    // })
+    .then(function (weatherData) {
+      //pass in data that we are expecting to get back from the fetch request
+      // console.log(weatherData.list[0].main.temp)
+    var currentTemp = document.createElement("h1");
+    currentTemp.textContent = weatherData.list[0].main.temp;
+    temp.append(currentTemp)
+    console.log(currentTemp);
+    })
+    .catch(error => console.log(error)) //to catch error
+
+  
+
+
 }
 
-searchFormEl.addEventListener('submit', handleSearchFormSubmit);
+
+
+searchBtn.addEventListener("click", locationSelect);
