969a91e54b2becbcad1a507cae3f954f52d08928
Jake Melville <jakemelville@ymail.com>
2021-02-19 14:16:16 -0500

    pulled the dates for the next five days from my api. Added them to my UL's in html

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,12 @@ var searchBtn = document.querySelector(".btn");
 var baseUrl = "https://api.openweathermap.org/data/2.5/forecast?q=";
 var apiKey = "&appid=c325d98bc2a304d27df0719cfbbccb4f";
 var units = "&units=imperial";
-var fiveHeader = document.getElementById("forcast")
+
 var date = document.getElementById("date");
 var timeDate = moment().format('MMMM Do YYYY, h:mma');
+// var tmrwDay = moment().add(1, 'days').calendar;
+// var followingDay = moment().add(2, 'days').calendar;
+
 
 var uvUrl = "https://api.openweathermap.org/data/2.5/onecall?"
 var lat = "lat=";
@@ -24,6 +27,7 @@ function locationSelect(event) {
   
 
   var searchInput = document.querySelector('#search-input').value;
+  var fiveHeader = document.getElementById("forcast");
   fiveHeader.textContent = "5 Day Forcast for " + searchInput + ":"
 
   if (!searchInput) {
@@ -43,7 +47,7 @@ function locationSelect(event) {
     .then(function (weatherData) {
       //pass in data that we are expecting to get back from the fetch request
       console.log(weatherData)
-
+// current conditions
       var city = document.getElementById("city");
       city.textContent = "Current conditions in " + weatherData.city.name + ":";
 
@@ -58,14 +62,34 @@ function locationSelect(event) {
 
       var currentUvIndex = document.getElementById("uvIndex");
       currentUvIndex.textContent = "UV Index: " + weatherData.list[0].humidity;
-
+//future conditions
       var today = document.getElementById("today");
-      var todayIcon = weatherData.list[0].weather[0].icon;
+      var todayDate = weatherData.list[0].dt_txt;
+      today.textContent = todayDate.slice(0, 10);
+
+      var tmrw = document.getElementById("tmrw");
+      var tmrwDate = weatherData.list[8].dt_txt;
+      tmrw.textContent = tmrwDate.slice(0, 10);
+
+      var followingDay = document.getElementById("two-days");
+      var followingDayDate = weatherData.list[16].dt_txt;
+      followingDay.textContent = followingDayDate.slice(0, 10);
+      
+      var threeDays = document.getElementById("three-days");
+      var threeDaysDate = weatherData.list[24].dt_txt;
+      threeDays.textContent = threeDaysDate.slice(0, 10);
+
+      var fourDays = document.getElementById("four-days");
+      var fourDaysDate = weatherData.list[32].dt_txt;
+      fourDays.textContent = fourDaysDate.slice(0, 10);
+
+
+      // var todayIcon = weatherData.list[0].weather[0].icon;
       // if (todayIcon = "04d") {
       //   today.textContent = todayIcon;
       // }
       console.log("today icon:", todayIcon);
-      today.textContent = "Todays Weather : " + weath
+      today.textContent = "Todays Weather : " + weather
     })
     .catch(error => console.log(error)) //to catch error
 
