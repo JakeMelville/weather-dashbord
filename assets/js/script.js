3ed191b12df22a3746de2bf0af670e2787c9b911
Jake Melville <jakemelville@ymail.com>
2021-02-20 12:31:17 -0500

    added the dates to the forloop. Sliced the date so it doesnt show to the minute. Centered all content in css

--- assets/js/script.js
+++ assets/js/script.js
@@ -66,38 +66,36 @@ function locationSelect(event) {
 //future conditions
 
       for (var i = 0; i <= 32; i += 8) {
+
+        document.getElementById(`day-${i}`).textContent = weatherData.list[i].dt_txt.slice(5, 10);    
         document.getElementById(`icon-${i}`).textContent = weatherData.list[i].weather[0].icon;
         document.getElementById(`degrees-${i}`).textContent = "Temp: " + weatherData.list[i].main.temp + "F";
         document.getElementById(`humid-${i}`).textContent = "Humidity: " + weatherData.list[i].main.humidity;
         
         // console.log(weatherData.list[i])
       }
-//today
-//       var today = document.getElementById("today");
+// // today
+//       var today = document.getElementById("day-0");
 //       var todayDate = weatherData.list[0].dt_txt;
 //       today.textContent = todayDate.slice(5, 10);
 
-// //tmrw
-//       var tmrw = document.getElementById("tmrw");
+// // //tmrw
+//       var tmrw = document.getElementById("day-8");
 //       var tmrwDate = weatherData.list[8].dt_txt;
 //       tmrw.textContent = tmrwDate.slice(5, 10);
-      
-//       var twoDegrees = document.getElementById("two-degrees");
-//       twoDegrees.textContent = weatherData.list[8].main.temp;
-//       console.log(twoDegrees);
+    
 // //two-days
-//       var followingDay = document.getElementById("two-days");
+//       var followingDay = document.getElementById("day-16");
 //       var followingDayDate = weatherData.list[16].dt_txt;
 //       followingDay.textContent = followingDayDate.slice(5, 10);
 // //three-days      
-//       var threeDays = document.getElementById("three-days");
+//       var threeDays = document.getElementById("day-24");
 //       var threeDaysDate = weatherData.list[24].dt_txt;
 //       threeDays.textContent = threeDaysDate.slice(5, 10);
 // //four-days
-//       var fourDays = document.getElementById("four-days");
+//       var fourDays = document.getElementById("day-32");
 //       var fourDaysDate = weatherData.list[32].dt_txt;
 //       fourDays.textContent = fourDaysDate.slice(5, 10);
-
 
     })
     .catch(error => console.log(error)) //to catch error
